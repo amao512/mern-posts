1c4361c3658bee7a32dd9d1938d6bd1b2b35a60d
aslnstbk <asyl.zed.han@gmail.com>
2020-06-21 20:28:19 +0600

    changed deploying method

--- server.js
+++ server.js
@@ -28,9 +28,13 @@ const connectDB = async () => {
 if(process.env.NODE_ENV === 'production'){
     app.use(express.static(path.join(__dirname, 'client/build')))
 
+    // app.get('*', (req, res) => {
+    //     res.sendFile(path.resolve(__dirname, 'client/build/index.html'))
+    // })
+
     app.get('*', (req, res) => {
-        res.sendFile(path.resolve(__dirname, 'client/build/index.html'))
-    })
+        res.sendFile(path.join(__dirname + '/client/build/index.html'));
+    });
 }
 
 connectDB()
