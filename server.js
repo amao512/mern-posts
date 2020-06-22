2c3b100fd0fb006426eaf5b764183eaa71bd3367
aslnstbk <asyl.zed.han@gmail.com>
2020-06-22 14:53:57 +0600

    added dotenv

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 const express = require('express')
 const mongoose = require('mongoose')
-const config = require('config')
 const path = require('path')
 const bodyParser = require('body-parser')
+require('dotenv').config()
 
 const app = express()
 const PORT = process.env.PORT || 5000
@@ -11,7 +11,7 @@ app.use(bodyParser.json())
 
 const connectDB = async () => {
     try {
-        await mongoose.connect(config.get('mongoUri'), {
+        await mongoose.connect(process.env.MONGO_URI, {
             useNewUrlParser: true,
             useUnifiedTopology: true,
             useCreateIndex: true,
